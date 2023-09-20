25af05d1c5cac483eeb67a71ddc97ecf3ed02a4e
pratim dasude <pratim4dasude@gmail.com>
2023-09-20 21:24:12 +0530

    Day 1 > componend Rating as API

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
 
 import StarRating from "./StartRating";
 
+function Test() {
+  const [movie, setMovie] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovie} />;
+      <p>Movie is {movie}</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    {/* <StarRating maxRating={10} />
-    <StarRating/> */}
+    <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okay", "good", "amazing"]}
+    />
+    {/* <StarRating maxRating={10} /> */}
+    <StarRating size={88} color="green" className="test" defaultRating={3} />
+
+    <Test />
   </React.StrictMode>
 );
